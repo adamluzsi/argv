9021fcdba7b115a862a71d9c8bf1ed10f31562c9
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-04-20 07:40:03 +0200

    update ARGV methods with multi input case

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ With this extension module, you can parse in an easy way the script input tags
 
 ```ruby
 
+    require 'argv'
+
     #> $ script.rb --test this --hello world --sup
 
     puts ARGV.flag_tags
@@ -15,5 +17,44 @@ With this extension module, you can parse in an easy way the script input tags
 
     puts ARGV.flags.inspect
     # ["--test", "--hello", "--sup"]
+
+```
+
+```ruby
+
+
+    #> $ script.rb  --test this --hello world --su-p asd sdsd -asd -dsa
+
+    puts ARGV.values.inspect
+    #> ["this", "world", "asd", "sdsd"]
+
+    puts ARGV.flagtags sym_key: true
+    #> {:test=>"this", :hello=>"world", :"su-p"=>"asd"}
+
+
+```
+
+```ruby
+
+
+    #> $ script.rb  --test this this multi value --hello world asdf
+
+    puts ARGV.values.inspect
+    #> ["this", "world", "asd", "sdsd"]
+
+    puts ARGV.flagtags sym_key: true
+    #> {:test=>"this", :hello=>"world", :"su-p"=>"asd"}
+
+
+```
+
+### multi value use case
+
+```ruby
+
+    #> $ script.rb --test this --hello world --su-p asd sdsd -asd -dsa
+
+    puts ARGV.flag_multi_value.inspect
+    #> {"--test"=>["this"], "--hello"=>["world"], "--su-p"=>["asd", "sdsd"]}
 
 ```
--- lib/argv.rb
+++ lib/argv.rb
@@ -1,6 +1,32 @@
 module ARGVEXT
 
-  def flag_tags
+  def flag_tags opts= {}
+    raise(ArgumentError) unless opts.class <= Hash
+
+    opts[:sym_key] ||= opts[:sym] || opts[:sk] || opts[:s]
+
+    return_obj= {}
+    self.count.times do |index|
+
+      next if self[index+1].nil?
+      if self[index][0].include?('-') && !self[index+1][0].include?('-')
+        return_obj[( opts[:sym_key] ?  self[index].dup.to_s.gsub!(/^-*/,'').to_sym :  self[index] )]= self[index+1]
+      end
+
+    end
+    return return_obj
+
+  end
+  alias :flag_value :flag_tags
+  alias :flag_tag   :flag_tags
+  alias :flagtag    :flag_tags
+  alias :flagtags   :flag_tags
+
+  def flag_multi_value opts= {}
+    raise(ArgumentError) unless opts.class <= Hash
+
+    opts[:sym_key] ||= opts[:sym] || opts[:sk] || opts[:s]
+
 
     return_obj= {}
     self.count.times do |index|
@@ -8,7 +34,19 @@ module ARGVEXT
       begin
 
         if self[index][0].include?('-') && !self[index+1][0].include?('-')
-          return_obj[self[index]]= self[index+1]
+
+          new_element= []
+          index_at= index+1
+          loop do
+            if self[index_at].nil? || self[index_at].to_s[0].include?('-')
+              break
+            else
+              new_element.push(self[index_at])
+            end
+            index_at += 1
+          end
+          return_obj[( opts[:sym_key] ?  self[index].dup.to_s.gsub!(/^-*/,'').to_sym :  self[index] )]= new_element
+
         end
 
       rescue
@@ -18,15 +56,15 @@ module ARGVEXT
     return return_obj
 
   end
-  alias :flag_tag :flag_tags
-  alias :flagtag  :flag_tags
-  alias :flagtags :flag_tags
 
   def flags
-    self.select{ |e| e[0].include?('-') }
+    self.select { |e| e[0].include?('-') }
+  end
+
+  def values
+    self.select { |e| !e[0].include?('-') }
   end
 
 end
 
 ARGV.__send__ :extend,ARGVEXT
-
